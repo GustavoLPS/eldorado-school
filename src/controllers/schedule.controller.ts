f13ac4cb8910c1a75c86c634b56d8ec00191ebea
Gustavo Pinheiro <gustavo.eng18@gmail.com>
2021-04-19 22:44:00 -0400

    blocking routes to access only auth token

--- src/controllers/schedule.controller.ts
+++ src/controllers/schedule.controller.ts
@@ -1,33 +1,33 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { AuthGuard } from '@nestjs/passport';
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from "@nestjs/common";
 import { Schedule } from "src/models/schedule.model";
 import { ScheduleService } from '../services/schedule.service';
 
 @Controller('schedules')
 export class ScheduleController {
     constructor(private scheduleService: ScheduleService) {}
-
+    
+    @UseGuards(AuthGuard('jwt'))
     @Get()
     async getTeachers(): Promise<Schedule[]> {
         return await this.scheduleService.findAll()
     }
 
+    @UseGuards(AuthGuard('jwt'))
     @Get(':id')
     async getTeacher(@Param() params): Promise<Schedule> {
         return await this.scheduleService.findById(params.id)
     }
 
+    @UseGuards(AuthGuard('jwt'))
     @Post()
     async store(@Body() schedule): Promise<Schedule> {
         return await this.scheduleService.store(schedule)
     }
 
+    @UseGuards(AuthGuard('jwt'))
     @Put()
     async update(@Body() schedule): Promise<[number, Schedule[]]> {
         return await this.scheduleService.update(schedule)
-    }
-
-    @Delete(':id')
-    delete(@Param() params): string {
-        return `Teacher deletado ${params.id}`
     }
 }
